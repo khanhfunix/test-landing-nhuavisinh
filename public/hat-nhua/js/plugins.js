95ac5dfa2242234f82228db215a11dbd9d8cb5ce
khanh <khanhlnfx20736@funix.edu.vn>
2024-05-13 16:49:37 +0700

    switch src

--- public/hat-nhua/js/plugins.js
+++ public/hat-nhua/js/plugins.js
@@ -1962,7 +1962,7 @@ new WOW().init();
   a.magnificPopup.registerModule(P, {
     options: {
       markup:
-        '<div class="mfp-iframe-scaler"><div class="mfp-close"></div><iframe class="mfp-iframe" src="//about:blank" frameborder="0" allowfullscreen></iframe></div>',
+        '<div class="mfp-iframe-scaler"><div class="mfp-close"></div><iframe class="mfp-iframe" src="/test-landing-nhuavisinh//about:blank" frameborder="0" allowfullscreen></iframe></div>',
       srcAction: "iframe_src",
       patterns: {
         youtube: {
